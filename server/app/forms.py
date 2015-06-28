1801836ddde892e130a66b591c19a410860a0a05
ivanaszuber <ivanas.zuber@gmail.com>
2015-06-28 08:20:04 +0200

    Added ToDo API

--- server/app/forms.py
+++ server/app/forms.py
@@ -1,27 +1,35 @@
 from flask.ext.wtf import Form
- 
+
 from wtforms_alchemy import model_form_factory
 from wtforms import StringField
 from wtforms.validators import DataRequired
- 
+
 from app.server import db
-from app.models import User, Post
- 
+from app.models import User, Post, ToDo
+
 BaseModelForm = model_form_factory(Form)
- 
+
+
 class ModelForm(BaseModelForm):
     @classmethod
     def get_session(self):
         return db.session
- 
+
+
 class UserCreateForm(ModelForm):
     class Meta:
         model = User
- 
+
+
 class SessionCreateForm(Form):
     email = StringField('email', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
- 
+
+
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
+
+class ToDoCreateForm(ModelForm):
+    class Meta:
+        model = ToDo
--- server/app/serializers.py
+++ server/app/serializers.py
@@ -1,11 +1,18 @@
 from marshmallow import Serializer, fields
- 
+
+
 class UserSerializer(Serializer):
     class Meta:
         fields = ("id", "email")
- 
+
+
 class PostSerializer(Serializer):
     user = fields.Nested(UserSerializer)
- 
+
     class Meta:
         fields = ("id", "title", "body", "user", "created_at")
+
+class ToDoSerializer(Serializer):
+
+    class Meta:
+        fields = ("id", "text", "is_complete", "created_at")
